36b4df2367c1728817928b364898d917a87152c6
advait-soni <advaitsoni2020@gmail.com>
2025-04-14 19:45:38 +0530

    template

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import { Command } from "commander"
 
+const program = new Command()
+
+program
+    .argument('<csv>')
+    .action((csv) => {
+        console.log('csv = ', csv)
+    })
+
+
+program.parse()
