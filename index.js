ea1c51973759938480efdf0e2dce0d0d80f6d071
advait-soni <advaitsoni2020@gmail.com>
2025-04-14 19:59:58 +0530

    csv data

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { Command } from "commander"
+import fs from 'fs'
 
 const program = new Command()
 
 program
     .argument('<csv>')
     .action((csv) => {
-        console.log('csv = ', csv)
+        fs.readFile(csv, 'utf8', (err, data) => {
+            if(err){
+                console.error(`Error reading file ${err}`)
+            }
+            console.log('csv content' , data)
+        })
     })
 
 
